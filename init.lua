68bd3f71db295eb7d5cfe48a2ffa57d330da421d
Alonso-Developer <alonso@alonso>
2025-03-13 23:41:01 +0300

    100% cpu usage fix

--- init.lua
+++ init.lua
@@ -3,4 +3,9 @@ require('plugins')
 require('mappings')
 
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {}
+lspconfig.pyright.setup {
+    on_attach=on_attach,
+    root_dir = function()
+      return vim.fn.getcwd()
+    end,
+}
