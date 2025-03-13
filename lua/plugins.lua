4430adae8e6c1772acba19a986a2c03c0b826ea8
Alonso-Developer <alonso@alonso>
2025-03-13 23:41:01 +0300

    peek.nvim added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,61 +11,72 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
-	{
-		'jiangmiao/auto-pairs'	
-	},
-	{
-		'sainnhe/sonokai',
-		config = function()
-			require('colorscheme')
-		end
-	},
-	{
-		'ms-jpq/chadtree',
-		builds = 'chad',
-	},
-	{
-		'neoclide/coc.nvim',
-		branch = 'release',
+    {
+	    'jiangmiao/auto-pairs'	
+    },
+    {
+	    'sainnhe/sonokai',
+	    config = function()
+		    require('colorscheme')
+	    end
+    },
+    {
+	    'ms-jpq/chadtree',
+	    builds = 'chad',
+    },
+    {
+	    'neoclide/coc.nvim',
+	    branch = 'release',
 
 
-		config = function()
-		    require('pyright')
-		end
-	},
-	{
-		'neovim/nvim-lspconfig'
-	},
-	{
-		'nvim-lualine/lualine.nvim',
-		config = function()
-			require('statusline')
-		end
-	},
-	{
-		'nvim-tree/nvim-web-devicons'
-	},
-	{
-		'nvim-treesitter/nvim-treesitter',
-		config = function()
-			require('treesitter')
-		end
-	},
-	{
-		'numToStr/Comment.nvim',
-		config = function()
-			require('comment')
-		end
-	},
-	{
-		'romgrk/barbar.nvim',
-		dependencies = {
-			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-		},
-		init = function() vim.g.barbar_auto_setup = false end,
-		opts = {
-		},
-		version = '^1.0.0', -- optional: only update when a new 1.x version is released
-	  },
-})
+	    config = function()
+		require('pyright')
+	    end
+    },
+    {
+	    'neovim/nvim-lspconfig'
+    },
+    {
+	    'nvim-lualine/lualine.nvim',
+	    config = function()
+		    require('statusline')
+	    end
+    },
+    {
+	    'nvim-tree/nvim-web-devicons'
+    },
+    {
+	    'nvim-treesitter/nvim-treesitter',
+	    config = function()
+		    require('treesitter')
+	    end
+    },
+    {
+	    'numToStr/Comment.nvim',
+	    config = function()
+		    require('comment')
+	    end
+    },
+    {
+	    'romgrk/barbar.nvim',
+	    dependencies = {
+		    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+		    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+	    },
+	    init = function() vim.g.barbar_auto_setup = false end,
+	    opts = {
+	    },
+	    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+      },
+      {
+	    "toppair/peek.nvim",
+	    event = { "VeryLazy" },
+	    build = "deno task --quiet build:fast",
+	    config = function()
+		require("peek").setup()
+		-- refer to `configuration to change defaults`
+		vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+		vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+	    end,
+      },
+      })
