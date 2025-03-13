a0fd6b927f6bc0a4ca4a38da1c6bad7f9cdc7702
Alonso-Developer <alonso@alonso>
2025-03-13 23:41:01 +0300

    minor changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,23 @@ setkey('n', '<Leader>.', '<Cmd>BufferNext<CR>', opts)
 setkey('n', '<Leader>d', '<Cmd>BufferClose<CR>', opts)
 setkey('t', '<Esc>', '<C-\\><C-n>', opts)
 
+
+local setkey = vim.keymap.set
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+
+
+setkey("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+setkey("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+setkey("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+setkey("i", "<Leader>s", "<Plug>(coc-snippets-expand-jump)")
+setkey("i", "<Leader>q", "coc#refresh()", {silent = true, expr = true})
+setkey("n", "gd", "<Plug>(coc-definition)", {silent = true})
+setkey("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+setkey("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+setkey("n", "gr", "<Plug>(coc-references)", {silent = true})
+setkey("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+setkey("x", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
+setkey("n", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
+
+
+
--- lua/pyright.lua
+++ lua/pyright.lua
@@ -1,20 +1,3 @@
-local setkey = vim.keymap.set
-local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-
-
-setkey("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
-setkey("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
-setkey("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
-setkey("i", "<Leader>s", "<Plug>(coc-snippets-expand-jump)")
-setkey("i", "<Leader>q", "coc#refresh()", {silent = true, expr = true})
-setkey("n", "gd", "<Plug>(coc-definition)", {silent = true})
-setkey("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
-setkey("n", "gi", "<Plug>(coc-implementation)", {silent = true})
-setkey("n", "gr", "<Plug>(coc-references)", {silent = true})
-setkey("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
-setkey("x", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
-setkey("n", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
-
 function _G.check_back_space()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
